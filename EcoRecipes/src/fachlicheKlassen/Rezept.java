0df55f8be599d2732443ddeb54d88e9798df54aa
Error404 <Error404@test.com>
2014-04-29 14:04:53 +0200

    Finished with Rezept class

--- EcoRecipes/src/fachlicheKlassen/Rezept.java
+++ EcoRecipes/src/fachlicheKlassen/Rezept.java
@@ -1,10 +1,43 @@
 package fachlicheKlassen;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Rezept {
 	private int rez_id;
 	private String autor;
 	private String name;
 	private String zubereitung;
+	private List<Zutaten> zutaten;
+	
+	public Rezept(String autor, String name, String zubereitung, List<Zutaten> zutaten) {
+		// TODO Auto-generated constructor stub
+		this.autor = autor;
+		this.name = name;
+		this.zubereitung = zubereitung;
+		this.zutaten = zutaten;
+	}
+
+	public void setRez_id(int rez_id) {
+		this.rez_id = rez_id;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setZubereitung(String zubereitung) {
+		this.zubereitung = zubereitung;
+	}
+
+	public void setZutaten(List<Zutaten> zutaten) {
+		this.zutaten = zutaten;
+	}
+	
 	
 	
 }
