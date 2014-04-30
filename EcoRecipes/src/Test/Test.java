184e92137cc9fc254f96f0ad67ed03867fcc5add
kamatz <matthias.kapferer@student.uibk.ac.at>
2014-04-30 15:43:31 +0200

    die ersten paar junit tests, sind halt nicht all zu gut da wir ja noch kaum code haben~

---
+++ EcoRecipes/src/Test/Test.java
@@ -0,0 +1,53 @@
+package Test;
+
+import static org.junit.Assert.*;
+import fachlicheKlassen.Rezept;
+import fachlicheKlassen.User;
+
+public class Test {
+
+	Rezept expected[];
+	Rezept actual[];
+	User friend = new User("kamatz", "abcde", "ab@cd.at");
+
+	@org.junit.Test
+	public void register() {
+		fail("Not yet implemented");
+		// assertTrue(true);
+	}
+
+	public void addRecipe() {
+		fail("Not yet implemented");
+		// assertTrue(true);
+	}
+
+	public void rateRecipe() {
+		fail("Not yet implemented");
+		// assertTrue(true);
+	}
+
+	public void searchRecipe() {
+		fail("Not yet implemented");
+		// assertArrayEquals(expected, actual);
+	}
+
+	public void sortRecipe() {
+		fail("Not yet implemented");
+		// assertArrayEquals(expected, actual);
+	}
+
+	public void calcCO2() {
+		fail("Not yet implemented");
+		// assertEquals(expected, actual, delta);
+	}
+
+	public void addFriend() {
+		fail("Not yet implemented");
+		// assertTrue(true);
+	}
+
+	public void shareRecipe() {
+		fail("Not yet Implemented");
+		// assertTrue(true);
+	}
+}
