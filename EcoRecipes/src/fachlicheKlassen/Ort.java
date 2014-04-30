19b1aeb2ebd6d12b44d98bce9285de6532f4cddb
blabablub <test@blub.com>
2014-04-30 20:25:41 +0200

    Test

--- EcoRecipes/src/fachlicheKlassen/Ort.java
+++ EcoRecipes/src/fachlicheKlassen/Ort.java
@@ -5,6 +5,17 @@ public class Ort {
 	private String OrtName;
 	private int PLZ;
 	private String Land;
-	
+	public void setOrtId(int ortId) {
+		OrtId = ortId;
+	}
+	public void setOrtName(String ortName) {
+		OrtName = ortName;
+	}
+	public void setPLZ(int pLZ) {
+		PLZ = pLZ;
+	}
+	public void setLand(String land) {
+		Land = land;
+	}
 	
 }
